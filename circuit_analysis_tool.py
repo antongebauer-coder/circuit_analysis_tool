92e30c0bbf257f9865ba9301bca3cfe25e046f08
Jakub Waschow <jakub.waschow@gmail.com>
2026-03-09 22:07:47 +0100

    added header

---
+++ circuit_analysis_tool.py
@@ -0,0 +1,13 @@
+'''
+Projekt: Analyse elektrischer Netzwerke
+Autoren: Anton Gebauer, Anna-Maria Hartfelder, Jakub Waschow
+Datum: 09.03.2026
+Version: 0.1
+
+Beschreibung:
+Dieses Konsolenprogramm dient zur Analyse einfacher linearer
+Gleichstromnetzwerke. Es unterstützt das Knotenpotentialverfahren
+sowie die Maschenstromanalyse für kleine Schaltungen mit
+Widerständen und Spannungsquellen.
+'''
+
