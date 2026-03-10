c237e59d51616707e3a2c69aeb14b39d374f7e0b
Anna-Maria <anna-maria.hartfelder@outlook.de>
2026-03-10 17:39:58 +0100

    Ich habe jetzt das erste Grundgerüst für die Leitwertmatrix für Maschen als auch Knoten und nohmal die Reihenfolge von den Funktionen geändert

--- circuit_analysis_tool.py
+++ circuit_analysis_tool.py
@@ -45,7 +45,84 @@ def input_float(prompt):
     value = float(input(prompt))
     return value
 
+#-------------------------------------------------------------
 
+def mesh_analysis():
+    pass
+
+#-------------------------------------------------------------
+
+def nodal_analysis():
+    pass
+
+#-------------------------------------------------------------
+
+def solve_linear_system(A, b):
+
+   #Löst das lineare Gleichungssystem A * x = b.
+   x = np.linalg.solve(A, b)
+   return x
+
+#-------------------------------------------------------------
+
+def build_simple_mesh_matrix(R_self, R_shared):
+    """
+    Erstes Grundgerüst für eine Maschenmatrix.
+    R_self: Liste der Eigenwiderstände
+    R_shared: Matrix der gemeinsamen Widerstände
+    """
+    # Zählt die Anzahl der Maschen basierend auf der Liste der Eigenwiderstände
+    Number_of_meshes = len(R_self)
+
+    # Leere quadratische Masche erzeugen für die Widerstände
+    A = np.zeros((Number_of_meshes, Number_of_meshes))
+
+    # Füllt die Maschenmatrix basierend auf den Eigenwiderständen und den gemeinsamen Widerständen
+    for i in range(Number_of_meshes):
+        A[i, i] = R_self[i] + np.sum(R_shared[i])
+        for j in range(Number_of_meshes):
+           
+            # Wenn i und j nicht gleich sind, wird der gemeinsame Widerstand von Masche i und j subtrahiert
+            if i != j:
+                A[i, j] = -R_shared[i][j]
+
+    # Gibt die Maschenmatrix zurück
+    return A
+
+#-------------------------------------------------------------
+
+def build_simple_nodal_matrix(R_between, R_ground):
+    """
+    Erstes Grundgerüst für eine Leitwertmatrix.
+    R_between: Matrix der Widerstände zwischen Knoten
+    R_ground: Liste der Widerstände zur Masse
+    """
+    # Zählt die Anzahl der Knoten basierend auf der Liste der Widerstände zur Masse
+    Number_of_nodes = len(R_ground)
+
+    # Leere quadratische Matrix für die Leitwerte erzeugen
+    G = np.zeros((Number_of_nodes, Number_of_nodes))
+
+    # Füllt die Leitwertmatrix basierend auf den Widerständen zwischen den Knoten und den Widerständen zur Masse
+    for i in range(Number_of_nodes):
+        sum_g = 0 
+        for j in range(Number_of_nodes):  
+        
+            if i != j and R_between[i][j] > 0:
+                gij = 1 / R_between[i][j]
+                G[i, j] = -gij
+                sum_g += gij
+
+        if R_ground[i] > 0:   # Wenn es einen Widerstand zur Masse gibt, wird der Leitwert addiert
+            sum_g += 1 / R_ground[i]
+
+        G[i, i] = sum_g 
+
+    # Gibt die Leitwertmatrix zurück
+    return G
+
+
+###########################################################################################################
 
 def main():
     print("Wähle Methode:")
@@ -56,6 +133,8 @@ def main():
         mesh_analysis()
     else:
         nodal_analysis()
+
+#############################################################################################################
 
 if __name__ == "__main__":
     main()
